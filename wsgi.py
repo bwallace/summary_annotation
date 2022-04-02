8a73f533bfeb3416164f98d2879595107f7537ae
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2022-04-02 16:30:11 -0400

    annotations

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 #from gevent.pywsgi import WSGIServer
-from app import app
+from app_struct_sum import app
 #http_server = WSGIServer(('', 5000), app)
 #http_server.serve_forever()
 if __name__ == "__main__":
